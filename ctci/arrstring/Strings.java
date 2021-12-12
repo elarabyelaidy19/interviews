a8faa9b78567f4c3143d183b23589f3adacf6698
elarabyelaidy19 <elarabyelaidy@gmail.com>
2021-12-12 16:53:44 +0200

    plindrome permutation

--- ctci/arrstring/Strings.java
+++ ctci/arrstring/Strings.java
@@ -2,6 +2,8 @@ package ctci.arrstring;
 
 import java.util.Arrays;
 
+import javax.swing.plaf.TextUI;
+
 public class Strings { 
     public boolean isUnique(String str) { 
         if(str.length() > 128) // 128 indecates the number of unique chars in ascii code  
@@ -64,7 +66,8 @@ public class Strings {
         }
 
         index = actualLength + spaceCount * 3; 
-        for(i = actualLength - 1; i >= 0; i--) { 
+        for(i = actualLength - 1; i >= 0; i--) {  // O(N) Time, O(N) space
+
             if(str[actualLength] == ' ') { 
                 str[index - 1] = '0'; 
                 str[index - 2] = '2'; 
@@ -75,6 +78,48 @@ public class Strings {
                 i--;
             }
         }
+    } 
+
+    boolean blindromePermutation(String s) { 
+        int[] table = frquencyTable(s);  
+        return checkMaxOneOdd(table);
+
+    } 
+
+    boolean checkMaxOneOdd(int[] table) { 
+        boolean flag = false; 
+        for(int count : table) { 
+            if(count % 2 == 1) { 
+                if(flag) { 
+                    return false;
+                }
+                flag = true;
+            }
+        }
+        return true;
+    }
+
+    int getCharNumber(Character c) { 
+        int a = Character.getNumericValue('a'); 
+        int z = Character.getNumericValue('z'); 
+        int val = Character.getNumericValue(c); 
+
+        if(a <= val && val <= z) {
+            return val - a;  
+        }
+        return -1;
+    }
+
+    int[] frquencyTable(String str) { 
+        int[] table = new int[Character.getNumericValue('z') - Character.getNumericValue('a')]; 
+        for(char c : str.toCharArray()) { 
+            int x = getCharNumber(c); 
+
+            if(x != -1) { 
+                table[x]++;
+            }
+        }
+        return table;
     }
 
 
