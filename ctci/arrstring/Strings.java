0359ca93def490847998bb9b1d3bec3c6a6a0d88
elarabyelaidy19 <elarabyelaidy@gmail.com>
2021-12-12 16:09:28 +0200

    is permutation

--- ctci/arrstring/Strings.java
+++ ctci/arrstring/Strings.java
@@ -1,5 +1,6 @@
 package ctci.arrstring;
 
+import java.util.Arrays;
 
 public class Strings { 
     public boolean isUnique(String str) { 
@@ -15,6 +16,42 @@ public class Strings {
             charSet[val] = true; // set the value of the index to true 
         } 
         return true;
+    } 
+
+    public boolean isPermutauion(String s, String t) { 
+        if(s.length() != t.length()) 
+            return false;
+
+        return sort(s).equals(sort(t));
+    }
+
+    public String sort(String s) { 
+        char[] chars = s.toCharArray(); 
+        Arrays.sort(chars); 
+        return new String(chars);
+
+    } 
+
+    boolean isPermutauion2(String s, String t) { 
+        if(s.length() != t.length()) 
+            return false; 
+
+        int[] letters = new int[128]; 
+        char[] char_set = s.toCharArray(); 
+
+        for(char c : char_set) 
+            letters[c]++;  // frquency array
+        
+        for(int i = 0; i < t.length(); i++) { 
+            int c = (int) t.charAt(i); 
+            letters[c]--; 
+            if(letters[c] < 0) {
+                return false;
+        
+            }
+        }
+        return true;
+
     }
 
 
