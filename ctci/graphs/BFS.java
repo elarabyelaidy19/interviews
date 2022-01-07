995e95380d586f878169f03ec15d295cd69a1c9d
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-01-07 23:33:33 +0200

    List of depth

--- ctci/graphs/BFS.java
+++ ctci/graphs/BFS.java
@@ -37,4 +37,4 @@ public class BFS {
         } 
         return false;
     }
-}
+}  
---
+++ ctci/trees/ListOfDepth.java
@@ -0,0 +1,18 @@
+package ctci.trees;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import javax.swing.tree.TreeNode;
+
+public class ListOfDepth {
+    ArrayList<LinkedList<TreeNode>> createLevelLinkedList(TreeNode root) { 
+        ArrayList<LinkedList<TreeNode>> result = new ArrayList<LinkedList<TreeNode>>(); 
+        LinkedList<TreeNode> current = new LinkedList<TreeNode>(); 
+        if(root != null) { 
+            current.add(root);
+        }
+
+        while
+    }
+}
