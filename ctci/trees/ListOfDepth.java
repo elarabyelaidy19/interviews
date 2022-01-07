b69379488aaca4a031cc9ba8adab041ba897d6c4
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-01-07 23:37:13 +0200

    List of depth

--- ctci/trees/ListOfDepth.java
+++ ctci/trees/ListOfDepth.java
@@ -13,6 +13,20 @@ public class ListOfDepth {
             current.add(root);
         }
 
-        while
+        while(current.size() > 0) { 
+            result.add(current); 
+            LinkedList<TreeNode> parents = current; 
+            current = new LinkedList<TreeNode>();
+            for(TreeNode parent : parents) { 
+                if(parent.left != null) { 
+                    current.add(parent.left);
+                }
+                if(parent.right != null) { 
+                    current.add(parent.right);
+                }
+            }
+
+        }
+        return result;
     }
 }
