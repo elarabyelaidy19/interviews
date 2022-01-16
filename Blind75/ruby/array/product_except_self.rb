adb2039cdecd51235af67edf0af98793d941b91e
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-01-16 14:53:59 +0200

    product of array except itself java

--- Blind75/ruby/array/product_except_self.rb
+++ Blind75/ruby/array/product_except_self.rb
@@ -6,15 +6,20 @@ def product_except_self(nums)
     len = nums.length - 1
     0.upto(len) do |i|
         result[i] = pre * nums[i] 
-        pre *= nums[i] 
-    end  
+        pre *= nums[i]  
+        
+    end   
+    puts result
     post = 1 
     len.downto(1) do |i|
         result[i] = result[i - 1] * post 
         post *= nums[i] 
     end 
+    puts result
     result[0] = post 
-    result
+    puts result
 end
 
+
+product_except_self([1,2,3,4,5])
 
