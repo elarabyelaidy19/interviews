20bfa7ba9b81f384b041d4b917de8c919912bed6
elarabyelaidy19 <64871842+elarabyelaidy19@users.noreply.github.com>
2022-01-19 21:34:50 +0200

    Update MaxSubarraySum.java

--- Blind75/java/array/MaxSubarraySum.java
+++ Blind75/java/array/MaxSubarraySum.java
@@ -14,4 +14,28 @@ public class MaxSubarraySum {
         }
         return max;
     }
+
+ 
+  
+ ​    ​public​ ​int​ ​maxSubArray​(​int​[] ​nums​) { 
+ ​         
+ ​        ​int​[] dp ​=​ ​new​ ​int​[nums​.​length]; 
+ ​         
+ ​        dp[​0​] ​=​ nums[​0​]; 
+ ​         
+ ​        ​int​ max ​=​ dp[​0​]; 
+ ​         
+ ​        ​for​(​int​ i ​=​ ​1​; i ​<​ nums​.​length; i​++​) { 
+ ​             
+ ​            dp[i] ​=​ nums[i] ​+​ (dp[i ​-​ ​1​] ​>​ ​0​ ​?​ dp[i ​-​ ​1​] ​:​ ​0​); 
+ ​             
+ ​            max ​=​ ​Math​.​max(dp[i], max); 
+ ​             
+ ​        } 
+ ​         
+ ​        ​return​ max; 
+ ​         
+ ​    } 
+  
+ ​}
 }
