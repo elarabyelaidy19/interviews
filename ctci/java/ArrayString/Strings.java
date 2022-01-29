a0d9433573f8c38b7ad3f496023d4e68b1de4a29
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-01-29 23:42:50 +0200

    is permutation java

--- ctci/java/ArrayString/Strings.java
+++ ctci/java/ArrayString/Strings.java
@@ -1,7 +1,10 @@
 package ctci.java.ArrayString;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+
+import javax.print.FlavorException;
 
 public class Strings {
     
@@ -31,6 +34,39 @@ public class Strings {
 
 
     //////////////////////////////////////////////////////// 
+
+    boolean permutation(String s, String t) {  
+        if(s.length() != t.length()) 
+            return false;
+
+        return sort(s).equals(sort(t));
+    }
+
+    String sort(String s) { 
+        char[] chars = s.toCharArray(); 
+        Arrays.sort(chars); 
+        return new String(chars);
+    }
+
+    boolean permutation2(String s, String t) { 
+        if(s.length() != t.length()) 
+            return false; 
+
+        int[] counter = new int[128]; 
+
+        for(int i = 0; i < s.length(); i++) { 
+            counter[s.charAt(i)]++;
+        }
+
+        for(int i = 0; i < t.length(); i++) { 
+            int curr = (int) t.charAt(i);
+            counter[curr]--; 
+            if(counter[curr] < 0) 
+                return false;
+        }
+        return true;
+    } 
+
 
 
     
