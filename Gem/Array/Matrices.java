2afec60662eddd70f4d6db9d0dd072fad2d62b53
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-03-23 19:55:27 +0200

    toelpitz matrix

--- Gem/Array/Matrices.java
+++ Gem/Array/Matrices.java
@@ -76,4 +76,14 @@ public class Matrices {
         return res.toArray(new String[0]);
     }
 
+
+    public boolean isToelpitzMatrix(int[][] matrix) { 
+        for(int i = 0; i < matrix.length; i++) { 
+            for(int j = 0; j < matrix[0].length; j++) { 
+                if(i+1 < matrix.length && j+1 < matrix[0].length && matrix[i][j] != matrix[i+1][j+1]) 
+                    return false;
+            }
+        }
+        return true;
+    }
 }
