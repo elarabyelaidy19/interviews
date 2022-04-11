24adf0a496553ce1e213e424ec8fd7aba450fc48
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-04-11 10:07:27 +0200

    calpoints

---
+++ Gem/Stack/Stacks.java
@@ -0,0 +1,28 @@
+package Gem.Stacks;
+
+public class Stacks { 
+    
+    public int calPoints(String[] ops) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String op : ops) {
+            if (op.equals("+")) {
+                int top = stack.pop();
+                int newTop = top + stack.peek();
+                stack.push(top);
+                stack.push(newTop);
+            } else if (op.equals("C")) {
+                stack.pop();
+            } else if (op.equals("D")) {
+                stack.push(2 * stack.peek());
+            } else {
+                stack.push(Integer.valueOf(op));
+            }
+        }
+        int res = 0;
+        for (int sc : stack)
+            res += sc;
+        return res;
+
+    }
+}
---
+++ Gem/Stack/stacks.md
@@ -0,0 +1,2 @@
+## ![baseball game](https://leetcode.com/problems/baseball-game/)
+- stack to perform operations according to each operation. 
