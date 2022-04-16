17cf9f92c8f6e7fd89b2a1f57b8f8eed20cc7e66
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-04-16 02:42:19 +0200

    convert bst tree

--- Gem/Trees/Trees.java
+++ Gem/Trees/Trees.java
@@ -33,4 +33,20 @@ public class Trees {
 
         return root;
     }
+
+
+    class Solution {
+        private int sum = 0;
+
+        public TreeNode convertBST(TreeNode root) {
+
+            if (root != null) {
+                convertBST(root.right);
+                sum += root.val;
+                root.val = sum;
+                convertBST(root.left);
+            }
+            return root;
+        }
+    }
 } 
--- Gem/Trees/Trees.md
+++ Gem/Trees/Trees.md
@@ -1,2 +1,5 @@
 ## [trim tree](https://leetcode.com/problems/trim-a-binary-search-tree/)
-- goes left or right depending on the value of the current node
+- goes left or right depending on the value of the current node 
+
+## [convert bst to greater tree](https://leetcode.com/problems/convert-bst-to-greater-tree/)
+- 
