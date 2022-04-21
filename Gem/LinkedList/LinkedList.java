9c57f55941d3e9afeccf3d7a274adb9b31ebbf45
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-04-22 00:57:59 +0200

    merge two sorted lists

--- Gem/LinkedList/LinkedList.java
+++ Gem/LinkedList/LinkedList.java
@@ -26,4 +26,23 @@ public class LinkedList {
         }
         return head;
     }
+
+    // ================================================== 
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+
+        curr.next = l1 != null ? l1 : l2;
+        return dummy.next;
+    }
 }
--- Gem/LinkedList/list.md
+++ Gem/LinkedList/list.md
@@ -2,4 +2,7 @@
 - change the node val and next. 
 
 ## [reverse a linkedlist](https://leetcode.com/problems/reverse-linked-list/)
-- dummy head store prev, and onew store next.
+- dummy head store prev, and onew store next. 
+
+## [merge two sorted linkedlists](https://leetcode.com/problems/merge-two-sorted-lists/) 
+- dummy head, while both are not null, compare val, if a < b, add a to result, else add b.
