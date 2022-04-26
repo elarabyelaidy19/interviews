17ea69b43a662b886284de9ccf0153ae9e087fed
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-04-26 22:22:23 +0200

    find median from data stream

--- Gem/PriorityQueue/PriorityQu.java
+++ Gem/PriorityQueue/PriorityQu.java
@@ -6,7 +6,6 @@ public class PriorityQu {
     // find kth largest ele in array
     public int kthLargestElement(int[] nums, int k) { 
         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a); 
-        
         for(int i = 0; i < nums.length; i++) 
             pq.add(nums[i]); 
 
@@ -16,5 +15,33 @@ public class PriorityQu {
             k--;
         }
         return val;
+    }
+
+    public int kthLargestElement2(int[] nums, int k) { 
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); 
+        for(int num : nums) { 
+            pq.add(num); 
+            if(pq.size() > k) // maintains the size of pq to be equal k
+                pq.poll();
+        }
+        return pq.peek();
+    }
+
+    // ================================================================= 
+
+    class MedianFinder { 
+        private PriorityQueue<Integer> small = new PriorityQueue<>((a, b) -> b - a);
+        private PriorityQueue<Integer> large = new PriorityQueue<>();
+
+        public void addNum(int num) {
+            large.offer(num);
+            small.offer(large.poll());
+            if (small.size() > large.size())
+                large.offer(small.poll());
+        }
+
+        public double findMedian() {
+            return large.size() > small.size() ? large.peek() : (small.peek() + large.peek()) / 2.0;
+        }
     }
 }
--- Gem/PriorityQueue/notes.md
+++ Gem/PriorityQueue/notes.md
@@ -3,4 +3,8 @@
 ## [find kth largest element in an array](https://leetcode.com/problems/kth-largest-element-in-an-array/)
 - build max heap of array, pool k largest elements. **O(NLogn)**
 
-
+## [find median from data stream](https://leetcode.com/problems/find-median-from-data-stream/)
+- maintain two heaps, one for smaller half and one for larger half. **O(NLogN)** 
+- maintain a max heap of size k, and a min heap of size k+1. 
+- if the size of the two heaps is equal, then the median is the average of the top of the two heaps peeks of two heaps. 
+- if the size of the two heaps is not equal, then the median is the top of the larger heap.
