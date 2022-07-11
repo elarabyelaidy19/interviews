2ebbb416a42c5cbe8535e7d680455986bb26e459
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-07-11 21:14:53 +0200

    first unique char

--- SetsMaps/Maps.java
+++ SetsMaps/Maps.java
@@ -22,5 +22,20 @@ public class Maps {
             result[i] = res.get(i);
         }
         return result;
+    } 
+
+    // ==========================================================
+    // index first unique character in a string
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1)
+                return i;
+        }
+        return -1;
     }
 }
