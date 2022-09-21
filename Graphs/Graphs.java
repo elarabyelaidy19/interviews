9eb81e8c06b77ad350df10ad0ceb21c26a1cd21e
elarabyelaidy19 <elarabyelaidy@gmail.com>
2022-09-21 18:54:03 +0200

    graphs problems

---
+++ Graphs/Graphs.java
@@ -0,0 +1,52 @@
+
+public class Graphs { 
+   
+    // ==================================================================================
+    public int numIslands(char[][] grid) {
+        int count = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+
+    public void dfs(char[][] grid, int r, int c) {
+        if (r >= 0 && c >= 0 && r < grid.length && c < grid[0].length && grid[r][c] == '1') {
+            grid[r][c] = '0';
+            dfs(grid, r + 1, c);
+            dfs(grid, r - 1, c);
+            dfs(grid, r, c + 1);
+            dfs(grid, r, c - 1);
+        }
+    }
+
+
+    // ============================================================================== 
+    // Flood Fill
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int color = image[sr][sc];
+        if (color != newColor)
+            dfs(image, sr, sc, color, newColor);
+        return image;
+    }
+
+    public void dfs(int[][] image, int r, int c, int color, int newColor) {
+        if (image[r][c] == color) {
+            image[r][c] = newColor;
+            if (r >= 1)
+                dfs(image, r - 1, c, color, newColor);
+            if (c >= 1)
+                dfs(image, r, c - 1, color, newColor);
+            if (r + 1 < image.length)
+                dfs(image, r + 1, c, color, newColor);
+            if (c + 1 < image[0].length)
+                dfs(image, r, c + 1, color, newColor);
+        }
+    }
+}
---
+++ Graphs/Problems.md
@@ -0,0 +1,6 @@
+# [num of islands](https://leetcode.com/problems/number-of-islands/) 
+- **Flood Fill** Algotithm  
+- DFS and count the number of connected components  
+
+# [Flooding](https://leetcode.com/problems/flood-fill/) 
+- **Flood Fill** Algotithm
